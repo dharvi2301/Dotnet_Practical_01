dabb10dd0bd4ceda79cb3ee32397a9200435cc98
dharvi_kaneriya <dharvikaneriya@gmail.com>
2025-03-16 10:16:50 +0530

    feature: Created the new console application for the debugging purposes.

---
+++ DebuggingApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebuggingApplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a string: ");
+
+            // Step a: Declare a string variable
+            string userResponse = Console.ReadLine();
+
+            // Step c: Add a debug point here
+            if (Convert.ToBoolean(int.Parse(userResponse)))
+            {
+                Console.WriteLine("The entered value is an integer");
+            }
+            Console.ReadLine();
+        }
+    }
+}
